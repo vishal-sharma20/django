f066f667ee6f6ab3ad9c3efa819e1f7e2c153dc3
vishal-sharma20 <vishal191513251@gmail.com>
2023-08-17 14:08:19 +0530

    django2

--- django2/project2/second_app/admin.py
+++ django2/project2/second_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from second_app.models import student
+from second_app.models import classroom
+from second_app.models import marks
+from second_app.models import teachers
+admin.site.register(student)
 
-# Register your models here.
+admin.site.register(classroom)
+admin.site.register(marks)
+admin.site.register(teachers)
